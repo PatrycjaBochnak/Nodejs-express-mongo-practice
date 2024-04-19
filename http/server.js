309beda2fe69210474ac11724dd19d44f3bf6f3d
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-04-19 23:30:01 +0200

    cwiczenia

---
+++ http/server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const port = process.env.PORT || 5500;
+
+// http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+//     res.end(`<h1>Dzień dobry</h1>
+//     <script src="./code.js"></script>`);
+// }).listen(5500, '127.0.0.1');
+
+http
+  .createServer((req, res) => {
+    res.end(`<h1>${req.url}</h1>`);
+  })
+  .listen(port, "127.0.0.1", () => {
+    console.log(`Serwer nasłuchuje na porcie ${port}`);
+  });
