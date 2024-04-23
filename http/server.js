884621323d2d8f0b4f911e10856d665cb037daca
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-04-23 22:55:47 +0200

    zadanie

--- http/server.js
+++ http/server.js
@@ -1,5 +1,4 @@
 const http = require("http");
-
 const port = process.env.PORT || 5500;
 
 // http.createServer((req, res) => {
@@ -10,11 +9,30 @@ const port = process.env.PORT || 5500;
 
 http
   .createServer((req, res) => {
-    if(req.url === "/") {
-      res.writeHead(200, {'Content-Type' : 'text/html charset=utf-8'})
-      res.end("<h1>Strona główna</h1>")
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    switch (req.url) {
+      case "/":
+        res.end("<h1>strona glowna</h1>");
+        break;
+      case "/users":
+        res.end("strona uzytkownikow");
+        break;
+      case "/api/users":
+        res.end("api");
+        break;
+      default:
+        res.end("Strona nie istnieje")
     }
-    // res.end(`<h1>${req.url}</h1>`);
+    // if(req.url === "/") {
+    //   res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+    //   res.end("<h1>Strona główna</h1>")
+    // } else if (req.url === "/users") {
+    //   res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+    //   res.end("<h1>Lista uzytkownikow</h1>")
+    // } else {
+    //   res.writeHead(404, {'Content-Type' : 'text/html; charset=utf-8'})
+    //   res.end(`<h1>Brak zawartosci</h1>`)
+    // }
   })
   .listen(port, "127.0.0.1", () => {
     console.log(`Serwer nasłuchuje na porcie ${port}`);
