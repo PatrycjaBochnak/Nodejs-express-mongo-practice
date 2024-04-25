a8a5c8326598cba99f9a908d03879551debae315
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-04-25 18:08:10 +0200

    express js zadaniaa

--- express js/app.js
+++ express js/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
@@ -9,8 +11,18 @@ app.listen(3001, () => {
 //     // const { name, surname } = req.query;
 //     // console.log('Hello' + name  + ' ' + surname);
 //     console.log(req.get('Referer'))
-// }) 
+// })
 
-app.get('/article/:id/:title', (req) => {
-    console.log(req.params)
-});
+// app.get('/article/:id/:title', (req) => {
+//     console.log(req.params)
+// });
+
+app.use(express.json());
+app.use(cookieParser());
+
+// app.post('/hello', (req, res) => {
+//     const {name, surname} = req.body;
+//     res.send('Witaj' + " " + name + " " + surname)
+// })
+
+app.use(express.static(path.join(__dirname, "static")));
